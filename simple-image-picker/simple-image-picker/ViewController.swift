0185d2f8c101bd622872704bcb5efeeb5f22c435
Carlsan <carlsankim.dev@gmail.com>
2026-06-29 14:13:11 +0800

    Clean up functions for controllers

--- simple-image-picker/simple-image-picker/ViewController.swift
+++ simple-image-picker/simple-image-picker/ViewController.swift
@@ -14,13 +14,41 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func experiment(sender: UIButton) {
-        print("Entered Experiment block")
-        let nextController = UIImagePickerController()
-        
-        present(nextController, animated: true, completion: nil)
+    @IBAction func imagePickerExperiment() {
+        let imagePickerController = UIImagePickerController()
+        self.present(imagePickerController, animated: true, completion: nil)
     }
-
+    
+    @IBAction func shareImageExperiment() {
+        
+        let dummyImage = UIImage()
+        let shareImageController = UIActivityViewController(
+            activityItems: [dummyImage],
+            applicationActivities: nil
+        )
+        self.present(shareImageController, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func alertModalExperiment() {
+        let controller = UIAlertController()
+        
+        controller.title = "Test Alert"
+        controller.message = "This is a test"
+        
+        let okAction = UIAlertAction(title: "ok", style: UIAlertAction.Style.default) {
+            action in self.dismiss(animated: true, completion: nil)
+            
+        }
+    
+//        Second Action sample
+//        let cancelAction = UIAlertAction(title: "cancel", style: UIAlertAction.Style.cancel) {
+//            action in self.dismiss(animated: true, completion: nil)
+//        }
+        
+        controller.addAction(okAction)
+        present(controller, animated:true, completion: nil)
+    }
 
 }
 
